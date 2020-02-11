4c04f14b80c58c20d7efec1d4059c1e7456fbebb
Georgemburu <snooppythedj@gmail.com>
2020-02-11 16:08:01 +0300

    Updates

---
+++ precache-manifest.a03fc256de7a09930c2852d20c426bab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69d45322fe21db5c2deb3f492f2efe6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "282a38300df6fa9686d1",
+    "url": "/static/css/main.7d4cac5e.chunk.css"
+  },
+  {
+    "revision": "36982d8a9629e730d54e",
+    "url": "/static/js/2.a78dca1d.chunk.js"
+  },
+  {
+    "revision": "282a38300df6fa9686d1",
+    "url": "/static/js/main.6fca9cc6.chunk.js"
+  },
+  {
+    "revision": "c20004d02af843957bfc",
+    "url": "/static/js/runtime-main.e2135336.js"
+  }
+]);
